738e4da90597db3f2cc4a2408e695dd1827a0c03
Dominic Shelton <frogamic@gmail.com>
2018-09-02 17:11:55 +1200

    Remove jquery dependency, add garden-ui buttons. Still WIP

--- zendesk-submit-expander.js
+++ zendesk-submit-expander.js
@@ -1,19 +1,84 @@
-let $buttonGroup = $('<div class="l-btn-group" id="zse-buttons"></div>');
-$(document).ready(() => {
-  // let observer = new MutationObserver((ml) => {});
-  // observer.observe(document.getElementById('ember4740'), {childList: true});
-  let status = $('button[data-garden-id="buttons.button"]:visible strong').text()
-  console.log(`current status is ${status}`);
-  $('button[data-garden-id="buttons.icon_button"]:visible').click();
-  console.log('iterating the children');
-  $('ul[data-garden-id="menus.menu_view"] strong').each((i, e) => {
-    let submit = $(e).text();
-    let $button = $(`<button class="c-btn" id="zse-btn-${submit}">${submit}</button>`);
-    if (status === submit) {
-      $button.addClass('is-selected');
+HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+
+const BUTTON_GROUP_GARDEN_ID = 'buttons.button_group_view';
+
+const newButton = (status, handler, selected = false) => {
+    console.log(selected);
+    const button = document.createElement('button');
+    button.classList.add('c-btn');
+    button.classList.add(`zse-status-${status}`);
+    if (selected) {
+        button.classList.add('is-selected');
     }
-    $buttonGroup.append($button);
-  });
-  console.log('inserting the group');
-  $('div[data-garden-id="buttons.button_group_view"]').hide().before($buttonGroup);
-});
+    button.innerHTML = `<span>${status}</span>`;
+    button.addEventListener('click', handler);
+    return button;
+};
+
+const newButtonGroup = (statuses = ['New', 'Open', 'Pending', 'Hold', 'Solved'], handlers = [console.log, console.log, console.log, console.log, console.log]) => {
+    const buttonGroup = document.createElement('div');
+    buttonGroup.classList.add('l-btn-group');
+    buttonGroup.classList.add('zse-group');
+    for (let i in statuses) {
+        buttonGroup.appendChild(newButton(statuses[i], handlers[i], statuses[i] === 'Open'));
+    }
+    return buttonGroup;
+};
+
+const submitExpander = (buttonGroup) => {
+    console.log('expanding button group now');
+    const submit = buttonGroup.querySelector('button[data-garden-id="buttons.button"]');
+    const expand = buttonGroup.querySelector('button[data-garden-id="buttons.icon_button"]');
+    buttonGroup.style.display = 'none';
+    buttonGroup.parentNode.appendChild(newButtonGroup());
+};
+
+const divReChecker = (mutations) => {
+    mutations.forEach((mutation) => {
+        console.log(mutation);
+        if (mutation.target.tagName === 'SPAN') {
+            let parent = mutation.target.parentNode;
+            while (!parent.dataset && !(parent.dataset.gardenId === BUTTON_GROUP_GARDEN_ID)) {
+                parent = parent.parentNode;
+            }
+            submitExpander(parent);
+        }
+    });
+};
+
+const divChecker = (div) => {
+    console.log('checking div');
+    if (div.dataset && div.dataset.gardenId === BUTTON_GROUP_GARDEN_ID) {
+        console.log('found the div');
+        submitExpander(div);
+        const observer = new MutationObserver(divReChecker);
+        observer.observe(div, { childList: true, subtree: true });
+    }
+};
+
+const buttonGroupFinder = (mutations) => {
+    mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+            if (node.nodeName === 'DIV') {
+                divChecker(node);
+            }
+        });
+    });
+};
+
+const mutationLoader = () => {
+    if (document.readyState === 'complete') {
+        console.log('Document is Ready!');
+        const observer = new MutationObserver(buttonGroupFinder);
+        observer.observe(document.getElementsByTagName('BODY')[0], { childList: true, subtree: true });
+        document.getElementsByTagName('DIV').forEach(divChecker);
+        return true;
+    } else {
+        return false;
+    }
+};
+
+if (!mutationLoader()) {
+    document.onreadystatechange = mutationLoader;
+}
