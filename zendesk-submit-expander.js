7276252cb17a2ad89d3b2102dd08e1bc89a0de7c
Dominic Shelton <frogamic@gmail.com>
2018-09-07 23:18:50 +1200

    Reliably expand, hide the default button using css

--- zendesk-submit-expander.js
+++ zendesk-submit-expander.js
@@ -4,7 +4,6 @@ HTMLCollection.prototype.forEach = Array.prototype.forEach;
 const BUTTON_GROUP_GARDEN_ID = 'buttons.button_group_view';
 
 const newButton = (status, handler, selected = false) => {
-    console.log(selected);
     const button = document.createElement('button');
     button.classList.add('c-btn');
     button.classList.add(`zse-status-${status}`);
@@ -16,63 +15,56 @@ const newButton = (status, handler, selected = false) => {
     return button;
 };
 
-const newButtonGroup = (statuses = ['New', 'Open', 'Pending', 'Hold', 'Solved'], handlers = [console.log, console.log, console.log, console.log, console.log]) => {
+const newButtonGroup = (statuses = ['New', 'Open', 'Pending', 'Hold', 'Solved'], handlers = [console.log, console.log, console.log, console.log, console.log], activeStatus = 'New') => {
     const buttonGroup = document.createElement('div');
     buttonGroup.classList.add('l-btn-group');
     buttonGroup.classList.add('zse-group');
     for (let i in statuses) {
-        buttonGroup.appendChild(newButton(statuses[i], handlers[i], statuses[i] === 'Open'));
+        buttonGroup.appendChild(newButton(statuses[i], handlers[i], statuses[i] === activeStatus));
     }
     return buttonGroup;
 };
 
 const submitExpander = (buttonGroup) => {
-    console.log('expanding button group now');
-    const submit = buttonGroup.querySelector('button[data-garden-id="buttons.button"]');
-    const expand = buttonGroup.querySelector('button[data-garden-id="buttons.icon_button"]');
-    buttonGroup.style.display = 'none';
-    buttonGroup.parentNode.appendChild(newButtonGroup());
-};
-
-const divReChecker = (mutations) => {
-    mutations.forEach((mutation) => {
-        console.log(mutation);
-        if (mutation.target.tagName === 'SPAN') {
-            let parent = mutation.target.parentNode;
-            while (!parent.dataset && !(parent.dataset.gardenId === BUTTON_GROUP_GARDEN_ID)) {
-                parent = parent.parentNode;
-            }
-            submitExpander(parent);
+    if (!buttonGroup.dataset.zseExpanded) {
+        const submit = buttonGroup.querySelector('button[data-garden-id="buttons.button"]');
+        const expand = buttonGroup.querySelector('button[data-garden-id="buttons.icon_button"]');
+        const currentStatus = submit.getElementsByTagName('STRONG')[0];
+        if (currentStatus.innerText) {
+            console.log('expanding button group now');
+            buttonGroup.dataset.zseExpanded = true;
+            buttonGroup.style.display = 'none';
+            buttonGroup.parentNode.appendChild(newButtonGroup(undefined, undefined, currentStatus.innerText));
+        } else {
+            const retry = new MutationObserver ((mutations) => {
+                mutations.forEach((mutation) => {
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.tagName === 'STRONG' && node.innerText) {
+                            console.log('retry observer');
+                            submitExpander(buttonGroup);
+                            retry.disconnect();
+                        }
+                    });
+                });
+            });
+            retry.observe(currentStatus.parentNode, {childList: true, characterData: true, subtree: true});
         }
-    });
-};
-
-const divChecker = (div) => {
-    console.log('checking div');
-    if (div.dataset && div.dataset.gardenId === BUTTON_GROUP_GARDEN_ID) {
-        console.log('found the div');
-        submitExpander(div);
-        const observer = new MutationObserver(divReChecker);
-        observer.observe(div, { childList: true, subtree: true });
     }
 };
 
 const buttonGroupFinder = (mutations) => {
     mutations.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-            if (node.nodeName === 'DIV') {
-                divChecker(node);
-            }
-        });
+        if (mutation.target.className === 'ember-view workspace') {
+            mutation.target.querySelectorAll(`div[data-garden-id="${BUTTON_GROUP_GARDEN_ID}"]`).forEach(submitExpander);
+        }
     });
 };
 
 const mutationLoader = () => {
     if (document.readyState === 'complete') {
-        console.log('Document is Ready!');
         const observer = new MutationObserver(buttonGroupFinder);
         observer.observe(document.getElementsByTagName('BODY')[0], { childList: true, subtree: true });
-        document.getElementsByTagName('DIV').forEach(divChecker);
+        document.querySelectorAll(`div[data-garden-id="${BUTTON_GROUP_GARDEN_ID}"]`).forEach(submitExpander);
         return true;
     } else {
         return false;
